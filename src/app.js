c3d073452a1f7ac45f766bbb44327c442e425f3a
Phillip Kim <phildkim@gmail.com>
2018-06-04 21:42:52 -0700

    submitting what i have

--- src/app.js
+++ src/app.js
@@ -46,13 +46,15 @@ const requestHandler = (req, res) => {
         res.end();
       }
 
-      else if(req.method === 'GET' && req.url.pathname === '/cowsay') {
-        console.log('the path is ', req.url.pathname);
+      else if (req.method === 'GET' && req.url.pathname === '/cowsay') {
+        // console.log('the path is ', req.url.pathname);
         res.setHeader('Content-Type', 'text/html');
         res.statusCode = 200;
         res.statusMessage = 'OK';
 
-        //let message = req.url.query.text;
+        let message = req.url.query.text;
+
+        console.log(message);
 
         res.write(
           `<!DOCTYPE html>
@@ -63,11 +65,15 @@ const requestHandler = (req, res) => {
             <body>
               <h1> cowsay </h1>
               <p>
-                please work!
+                Enter message.
               </p>
-              <pre>
-                <!-- cowsay.say({text: req.query.text}) -->
-              </pre>
+              <form>
+                <input type="text">
+              </form>
+              <br>
+              <button>
+                Submit
+              </button>
             </body>
           </html>`
         );
@@ -75,34 +81,19 @@ const requestHandler = (req, res) => {
         res.end();
       }
 
+      else if (req.method === 'POST' && req.url.pathname === '/api/cowsay') {
+        res.setHeader('Content-Type', 'text/json');
+        res.statusCode = 200;
+        res.statusMessage = 'OK';
+        res.write(JSON.stringify(req.body));
+        res.end();
+      }
+      //use cowsay as an NPM module - JB
+      else {
+
+      }
+
     });
-  //// In all cases, parse the URL
-  // parser(req)
-  //   .then(req => {
-
-  /* The "if" statements below are our "routes" and do the same things that express does (below) but 100% manually
-          app.get('/', (req,res) => res.send('Hello From the Gutter'));
-          app.get('/foo/bar/baz', (req,res) => res.send('Hello From the Gutter'));
-     */
-  //     if (req.method === 'GET' && req.url.pathname === '/') {
-  //       res.setHeader('Content-Type', 'text/html');
-  //       res.statusCode = 200;
-  //       res.statusMessage = 'OK';
-
-  //// Send out some random HTML (actually, it's not totally random. Note how it includes req.url.query.you ...
-  //// That would show whatever you have in the URL after you = (http://localhost:3000?this=that&you=cool
-  //       // let message = req.url.query.you;
-
-  //       let message = 'whassup';
-
-
-  //       res.write(`<!DOCTYPE html><html><body><h1>${message}</h1></body></html>`);
-
-  //// ... Instead of doing manual HTML like that, you could have used the "fs" module to read a file
-  //// and "res.write()" the contents of that file.
-  //       res.end();
-  //       return;
-  //     }
 
   //// Here, we have a "POST" request which will always return a JSON object.  That object will either be
   //// the JSON that you posted in (just spitting it back out), or an error object, formatted to look like JSON
