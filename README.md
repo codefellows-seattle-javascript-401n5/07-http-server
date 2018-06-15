34e365fe50e355b2bbdf0b52030570968a400ef4
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2018-06-15 08:24:15 -0700

    added stuff

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
-# HTTP Server
----
-More to come later...maybe June 12th.
+
+# 07: HTTP Server with Cowsay
+___
+###### TRAVIS: 
+###### HEROKU: 
+###### PR: 
+___
+This HTTP server uses the Cowsay module which will repeat what the user types.  
+
+### **In order to run my app, do the following:**
+
+**1.** Click the following link to test the GET request in the browser:  
+//heroku app
+To make a GET request in the browser type:/cowsay?text={message} at the end of the url.
+  * Change the id to any number greater than 0 and less that 2018.  It will return the number.
+  * Change the number outside of the parameters above ex: 2019.  It will return a 404 error and write 'not found' in the console.
+  * Leave the id query blank. It will return a 400 error and write 'bad request' in the console.
+
+**2.** Using HTTPie run the following commands in the terminal:
+
+http POST :3000/api/cowsay text=hello
+HTTP/1.1 200 OK
+Connection: keep-alive
+Content-Type: text/json
+Date: Fri, 15 Jun 2018 15:10:05 GMT
+Transfer-Encoding: chunked
+
+{
+    "content": " _______\n< hello >\n -------\n        \\   ^__^\n         \\ (oo)\\_______\n            (__)\\  )\\/\\\n                ||----w |\n              ||     ||"
+}
+
+**1.** 
--- src/app.js
+++ src/app.js
@@ -5,27 +5,17 @@ const http = require('http');
 const parser = require('./lib/parser');
 const cowsay = require('cowsay');
 const reqHandler = (req,res) => {
-  const makeHtmlResponse = (res);
+
   parser(req)
     .then(req => {
       if (req.method === 'GET' && req.url.pathname === '/') {
-        // res.setHeader('Content-Type', 'text/html');
-        // res.statusCode = 200;
-        // res.statusMsg = 'Okay';
-        makeHtmlResponse(res);
-        //send back html with a...
-        //let msg = req.url.query.you;//use this for cowsay?text...
-
-    
+        res.setHeader('Content-Type', 'text/html');
+        res.statusCode = 200;
+        res.statusMsg = 'Okay';
         let markup = `<!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Cowsay</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="app.js"></script>
+  <title>Cowsay</title>
 </head>
 <body>
     <header>
@@ -36,7 +26,7 @@ const reqHandler = (req,res) => {
         </nav>
       </header>
       <main>
-        <!-- project description -->
+      <p>This project uses the Cowsay module.  Type at the end of the browser this: "/cowsay?text=" plus your message and the cow will repeat it back to you.</p>
       </main>
 </body>
 </html> `;
@@ -44,28 +34,24 @@ const reqHandler = (req,res) => {
         res.end();
         return;
       } else if (req.method === 'GET' && req.url.pathname === '/cowsay') {
-        //cowsay with query text here!!!
-        //let queryText = 
-        //let cowMessage = 'I need something good to say';
+        res.setHeader('Content-Type', 'text/html');
+        res.statusCode = 200;
+        res.statusMsg = 'Okay';
+        let queryText = req.url.query.text || 'I need something good to say';
         let markup = `
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Cowsay</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
-</head>
-<body>
-        <h1> cowsay </h1>
-        <pre> 
-          ${cowsay.say({text: req.url.query.text})}
-        </pre>
-</body>
-</html>
-`;
+          <!DOCTYPE html>
+          <html>
+            <head>
+              <title>Cowsay</title>
+            </head>
+            <body>
+              <h1> cowsay </h1>
+              <pre> 
+                ${cowsay.say({text: queryText})}
+              </pre>
+            </body>
+            </html>
+          `;
         res.write(markup);
         res.end();
         return;
@@ -75,7 +61,7 @@ const reqHandler = (req,res) => {
         res.statusCode = 200;
         res.statusMessage = 'OK';
 
-        if(req.body.text) {
+        if(req.body) {
 
           res.write( JSON.stringify({ content: cowsay.say({text: req.body.text})}));
         } else {
@@ -83,8 +69,6 @@ const reqHandler = (req,res) => {
 
           res.write((JSON.stringify({error: 'invalid request: body required'})));
         }
-        //send back JSON version of cowsay message
-        //res.write( JSON.stringify(req.body) );
 
         res.end();
         return;
@@ -100,6 +84,7 @@ const reqHandler = (req,res) => {
 
     })
     .catch(err => {
+      console.log(err);
       res.writeHead(400);
       res.write((JSON.stringify({error: 'invalid request: body required'})));
       res.end();
